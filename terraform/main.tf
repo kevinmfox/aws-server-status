61b46f4328ba2aab0cf224297fe0f32a68b05138
kevinmfox <kevinmfox@hotmail.com>
2025-06-23 19:01:49 -0400

    updated script

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,6 +54,7 @@ module "us_east_1" {
     }
 }
 
+/*
 module "us_east_2" {
     source = "./modules/"
     for_each = local.vpcs_by_region["us-east-2"]
@@ -89,6 +90,7 @@ module "us_west_2" {
         aws = aws.us-west-2
     }
 }
+*/
 
 resource "aws_vpc" "vpc" {
     cidr_block = var.master_vpc_cidr
